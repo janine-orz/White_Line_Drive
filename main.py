ceb3a888cfcc68669fafa67a399b02018279f572
janine-orz <135732079+janine-orz@users.noreply.github.com>
2023-06-26 23:24:06 +0200

    26-06-2023

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import cv2
 import numpy as np
 import color_detector_004 as cd
+import color_det_part1 as cd1
+import color_det_hsv_red as cd2
 from sensor_msgs.msg import Image
 
 #cy_max_bot = cd.LinePos(img, height, width, i_bot)[0]
@@ -24,52 +26,6 @@ def print_line(Line, img):
         a = int(temp1[0])
         cv2.circle(img, (a, temp1[1]), 2, (0, 0, 255), 2)
 
-def checkagain(img, Line, height, width):
-    for j in range(1, 5, 1):
-        for i in range(0, len(Line)-(j+1), j):
-            Pnt1 = Line[i]
-            #print('i: ', i, '; Pnt1: ', Pnt1)
-            num1 = Pnt1[0]
-            Pnt2 = Line[i+j]
-            #print('i: ', i+j, '; Pnt2: ', Pnt2)
-            num2 = Pnt2[0]
-            num3 = Pnt2[1]
-            diff = num2 - num1
-            if(abs(diff) > j+2):
-                Line_corr = []
-                Line_temp = cd.LinePos(img, height, width, num3, 1)
-                for k in range(len(Line_temp)):
-                    Pnt3 = Line_temp[k]
-                    num4 = Pnt3[1]
-                    if(abs(num4 - num1) < j+2):
-                        ReFindLine(Line, num4)
-                        Line_corr.append(Pnt3)
-                        #to do:find out the all connected part of this width
-                print('Line_corr: ', Line_corr)
-                Pnt4 = Line_corr[0]
-                num5 = Pnt4[1]
-                Pnt5 = Line_corr[-1]
-                num6 = Pnt5[1]
-                temp = [int(cd.MidCal(num5, num6)), num3]
-                print('temp = ', temp)
-                Line[i+j] = temp
-                print('Line[', i+j, ']',Line[i])
-
-
-def ReFindLine(Line, num4):
-    length = len(Line) - 2
-    INDEX = cd.FindLine(Line, length)
-    for i in range(0, len(INDEX) - 1):
-        diff = abs(INDEX[i+1] - INDEX[i])
-        idx = INDEX[i+1]
-        for j in range(INDEX[i], idx, 1):
-            num7 = Line[j]
-            diff = num4 - num7
-            if(abs(diff) < 20):
-                Line_n = []
-                Line_n.append(num7)
-        return Line_n
-
 
 def main():
     PNGList = ["001.png", "002.png", "003.png", "004.png", "005.png", "006.png", 
@@ -78,55 +34,78 @@ def main():
                "030.png", "033.png", "034.png", "035.png"]
     n = len(PNGList)
     print("the amount of PNGList: ", n)
-    #for i in range(0, 10):
-    img = cv2.imread(PNGList[6])
-    print("imported image: ", PNGList[1])
+    # for i in range(0, n-1):
+    i = 18
+    img_cv = cv2.imread(PNGList[i])
+    print("\timported image", i, ": ", PNGList[i], '\n')
     #img_msg = cd.cv2_to_imgs2mg(cv_image)
     #img = cd.imgmsg_to_cv2(img_msg)
-    height, width, _ = img.shape
+    height, width, _ = img_cv.shape
     print('height = ', height)
     print('width = ', width)
 
-    i = 230 #225
     i_bot = height - 5
     i_mid = height - 40
-    width_n = width - 180
+    width_n = width - 260
+    i = width_n
 
-    out_pnt = np.float32([[0, 0], [0, height-1], 
-                            [width-1, height-1], [width-1, 0]])
+    out_pnt = np.float32([[0, 0],
+                        [0, height-1],
+                        [width-1, height-1],
+                        [width-1, 0]])
     #print(out_pnt)
-    inp_pnt = np.float32([[width_n - 58, i_mid], [width_n, i_bot],
-                            [width-15, i_bot], [300, i_mid]])
+    inp_pnt = np.float32([[width_n + 58, i_mid],
+                        [width_n + 8, i_bot],
+                        [width-1, i_bot],
+                        [300, i_mid]])
     #print(inp_pnt)
     maxw = width - 1 - width_n
     maxh = height - 1 - i_mid
     M = cv2.getPerspectiveTransform(inp_pnt, out_pnt)
-    img = cv2.warpPerspective(img, M, (width, height), flags = cv2.INTER_LINEAR)
+    img = cv2.warpPerspective(img_cv, M, (width, height), flags = cv2.INTER_LINEAR)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    height, width, _ = img.shape
-    print('height = ', height)
-    print('width = ', width)
+    #height, width, _ = img.shape
+    #print('height = ', height)
+    #print('width = ', width)
 
     minwid = 0
     maxwid = height
 
-    #Line = cd.LinePos(img, height, width, 202, 0)
-    '''
-    if(Line != []):
-        i = 0
-        for i in range(len(Line)):
-            Pnt = Line[i]
-            print(Pnt)
-            cv2.circle(img, (Pnt[1], Pnt[0]), 1, (255, 0, 0), 5)
-    '''
+    # Line = cd.LinePos(img, height, width, 150, 0)
+    # print(len(Line))
+
+    # if(Line != []):
+    #     i = 0
+    #     for i in range(len(Line)):
+    #         Pnt = Line[i]
+    #         # print(Pnt)
+    #         cv2.circle(img, (Pnt[1], Pnt[0]), 1, (0, 255, 0), 3)
+
+    # Liner = cd2.HSV_Red_Det(img_hsv, height, width, 0, 0)
+
+    # if(Liner != []):
+    #     i = 0
+    #     for i in range(len(Liner)):
+    #         Pntr = Liner[i]
+    #         # print(Pntr)
+    #         cv2.circle(img_hsv, (Pntr[1], Pntr[0]), 1, (255, 0, 0), 3)
 
     Line_n = cd.LineForm(img, height, width, minwid, maxwid)
-    checkagain(img, Line_n, height, width)
+    cd1.checkagain(img, Line_n, height, width)
 
     for i in range(len(Line_n)):
         Pnt = Line_n[i]
-        #print(Pnt)
+        print('i = ', i, '; Pos = ', Pnt)
         cv2.circle(img, (Pnt[0], Pnt[1]), 1, (255, 0, 0), 5)
+
+    # # Liner_n = cd2.LinerForm(img_hsv, height, width, minwid, maxwid)
+    # cd1.checkagain(img, Line_n, height, width)
+
+    # for i in range(len(Liner_n)):
+    #     Pnt = Line_n[i]
+    #     # print('i = ', i, '; pos: ', Pnt)
+    #     cv2.circle(img_hsv, (Pnt[0], Pnt[1]), 1, (0, 0, 255), 5)
 
     #print_line(Line, img)
     #LoC = cd.LineorCurve(Line_n)
@@ -136,11 +115,12 @@ def main():
     # 003.png: 0.9629943306809362
     # 001.png:
 
+    # cv2.imshow("IMG_CV", img_cv)
     cv2.imshow("IMG", img)
+    # cv2.imshow("IMG_HSV", img_hsv)
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
