5f3b0165b392b82a146ad74e33650fc37b9a62a1
janine-orz <135732079+janine-orz@users.noreply.github.com>
2023-08-06 17:54:06 +0200

    Add files via upload
    seperating in two line and follow the middle line, solve the problem if the robot cannot find the line, it will fill the line itselfs

--- color_detector.py
+++ color_detector.py
@@ -1,37 +1,12 @@
 import sys
 import cv2
+import time
 import numpy as np
 from sensor_msgs.msg import Image
 
 #write a LoC = 3 WHEN the robot is on the right way but need to correct a little bit
 
-def imgmsg_to_cv2(img_msg):
-    if img_msg.encoding != "bgr8":
-        rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding. Come change the code if you are actually trying to implement a new camera")
-    dtype = np.dtype("uint8")
-    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else'<')
-    image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we dont need to reorder the channels
-                              dtype = dtype, buffer = img_msg.data)
-    if img_msg.is_bigendian == (sys.byteorder == 'little'):
-        image_opencv = image_opencv.byteswap().newbyteorder()
-    return image_opencv
-
-
-def cv2_to_imgsmg(cv_image):
-    img_msg = Image()
-    img_msg.height = cv_image.shape[0]
-    img_msg.width = cv_image.shape[1]
-    img_msg.encoding = "bgr8"
-    img_msg.is_bigendian = 0
-    img_msg.data = cv_image.tobytes()
-    img_msg.step = len(img_msg.data)
-    return img_msg
-
-
-#def img_read():
-#img = cv2.imread("004.png")
-#height, width, _ = img.shape
-#    return img, height, width
+# TO DO: write a programm so that the robot can follow one line wenn there is no green line on the left
 
 def ColRec(img, i, j):
     color = "Undefined"
@@ -41,7 +16,7 @@ def ColRec(img, i, j):
     g_val = pixel_center[1]
     r_val = pixel_center[2]
 
-    if b_val > 170 and g_val > 170 and r_val > 170:
+    if b_val > 130 and g_val > 160 and r_val > 150:
         color = "White"
     elif b_val < 100 and g_val < 100 and r_val < 100:
         color = "Black"
@@ -66,249 +41,220 @@ def ColRec(img, i, j):
     return color
 
 
-def ColDet(img, height, width, i):
+def ColDet(img, height, width, i, string):
     Col = []
-    for j in range(width):
-        color = ColRec(img, i, j)
-        if color == "White":
-        #sucess to find the white color
-            temp = [i, j]
-            Col.append(temp)
-    return Col
+    if(string == 'White'):
+        for j in range(width - 161, width, 1):
+            color = ColRec(img, i, j)
+            # print('[', i, ',', j, '] = ', color)
+            if color == string:
+            #sucess to find the white color
+                temp = [i, j]
+                Col.append(temp)
+        return Col
+    elif(string == 'Green'):
+        for j in range(0, width - 159, 1):
+            color = ColRec(img, i, j)
+            # print('[', i, ',', j, '] = ', color)
+            if color == string:
+            #sucess to find the white color
+                temp = [i, j]
+                Col.append(temp)
+        return Col
 
 
-def LinePos(img, height, width, i, rr):
-    Line = []
-    length = 0
-    Line = ColDet(img, height, width, i)
-    #print(i)
-    # for i in range(len(Line)):
-    #     print(i, ':', Line[i])
-
-    #sucess to finde the white color in one line
-    length = len(Line) - 2
-    if(Line != [])and(rr == 0):
-        INDEX = FindLine(Line, length)
-        #print('INDEX = ', INDEX)
-        Line_N = DelRef(Line, INDEX)
-        #print('Line_N = ', Line_N)
-        return Line_N
-    elif(Line != [])and(rr == 1):
-        return Line
-
-
-def FindLine(Line, length):
-    INDEX = []
-    idx = 0
-    INDEX.append(idx)
-    #for idx in range(idx, len(Line)):
-    for idx in range(length + 2):
-        if(idx == length + 1):
-            idx_n = length + 1
-            INDEX.append(idx_n)
-            return INDEX
-        else:
-            idx_n = bubblesort(length, idx, Line)
-            #print('idx_n = ', idx_n)
-            if(idx == idx_n):
-                INDEX.append(idx_n)
-
-
-def DelRef(Line, INDEX):
-    Line_n = []
-    #print('INDEX = ', INDEX)
-    for i in range(0, len(INDEX)-1):
-        diff = abs(INDEX[i+1] - INDEX[i])
-        idx = INDEX[i+1]
-        #print('idx = ', idx)
-        #print('i = ', i, '; diff = ', diff)
-        while(diff >= 7 and diff < 28):
-            #print('in WHILE')
-            #print('len(INDEX) = ', len(INDEX))
-            num = Line[idx]
-            for k in range(len(Line)-1, -1, -1):
-                #print("k = ", k)
-                #if(num[1] < 320-k*5):
-                Line_n = []
-                for j in range(INDEX[i], INDEX[i+1]):
-                    #print('INDEX[i] = ', INDEX[i])
-                    #print('j = ', j+1, ';\tLine[j]', Line[j+1])
-                    Line_n.append(Line[j+1])
-                # print('Line_n = ', Line_n)
-            break
-    return Line_n
-
-
-def bubblesort(length, idx, Line):
-    j = idx
-    #while i >= idx and i <= length:
-    for i in range(idx, length + 1):
-        Pos1 = Line[i]
-        Num1 = Pos1[1]
-        Pos2 = Line[i+1]
-        Num2 = Pos2[1]
-        if((Num2 - Num1) == 1):
-            j += 1
-        else:
-            idx = j
-            return idx
-
-
-def LineForm(img, height, width, i_min, i_max):
+def LineForm(img, height, width, string1, string2, Wi_min, Wi_max, Gi_min, Gi_max):
+    # print('i_min, i_max =', i_min, i_max)
     L = []
-    Line_temp = []
-    for i in range (i_min, i_max):
-        #print('i = ', i, ', min = ', i_min, ', max = ', i_max)
-        Line_temp = LinePos(img, height, width, i, 0)
-        if(Line_temp == []):
-            L = []
-            return L
-        elif(Line_temp != []):
-            l = len(Line_temp)
-            a = Line_temp[0]
-            num1 = a[1]
-            b = Line_temp[l-1]
-            num2 = b[1]
-            if(num1 != None)and(num2 != None):
-                temp = [int(MidCal(num1, num2)), i]
-                # print('[', MidCal(a, b), ',', i, ']')
-                L.append(temp)
+    L1 = []
+    L2 = []
+    Line1_temp = []
+    Line2_temp = []
+    i_min = max(Wi_min, Gi_min)
+    i_max = min(Wi_max, Gi_max)
+    if(i_min == Gi_min): # The Greenline is shorter then the Whiteline
+        for h in range (Wi_min, Gi_min):
+            Line1_temp = ColDet(img, height, width, h, string1)
+            L1 = Lineform(Line1_temp, h, h, string1, img, height, width)
+            L2 = [0, h]
+            x_temp = MidCal(L1[0], L2[0])
+            temp = [x_temp, h]
+            L.append(temp)
+    for i in range(i_min, i_max):
+        # print('i = ', i, ', min = ', i_min, ', max = ', i_max)
+        # print(string1, "\n")
+        Line1_temp = ColDet(img, height, width, i, string1)
+        # print(string2, "\n")
+        Line2_temp = ColDet(img, height, width, i, string2)
+        # print('Line1_temp = ', Line1_temp, '\tLine2_temp = ', Line2_temp)
+        L1 = Lineform(Line1_temp, i, i, string1, img, height, width)
+        L2 = Lineform(Line2_temp, i, i, string2, img, height, width)
+        # print('\tL1 = ', L1, '\t\tL2 = ', L2)
+        x_temp = MidCal(L1[0], L2[0])
+        temp = [x_temp, i]
+        L.append(temp)
+    if(i_max == Gi_max): # The Greenline is shorter then the Whiteline
+        for k in range (Gi_max, Wi_max):
+            Line1_temp = ColDet(img, height, width, k, string1)
+            L1 = Lineform(Line1_temp, k, k, string1, img, height, width)
+            L2 = [0, k]
+            x_temp = MidCal(L1[0], L2[0])
+            temp = [x_temp, k]
+            L.append(temp)
     return L
 
-
-
-
-
-def RighorLeft(Line):
-    R = 0
-    L = 0
-    for i in range(len(Line) - 2):
-        if(Line[i] < Line[i+1]):
-            L += 1
-        elif(Line[i] > Line[i+1]):
-            R += 1
-    print('L = ', L)
-    print('R = ', R)
-    if(L < 8 or R > 0):
-        RoL = 0
-    elif(L >= 8):
-        RoL = 1
-    return RoL
-
+def Lineform(Line_temp, i, j, string, img, height, width):
+    # print('i = ', i, ', string = ', string)
+    C = []
+    if((Line_temp == [])or(Line_temp == None)):
+        print('i = ', i, 'i-1 = ', i-1, ', string = ', string)
+        Line_temp = ColDet(img, height, width, i-1, string)
+        temp = Lineform(Line_temp, i-1, j, string, img, height, width)
+        print("\t", temp)
+        return temp
+    elif((Line_temp != None)or(Line_temp != [])):
+        l = len(Line_temp)
+        a = Line_temp[0]
+        num1 = a[1]
+        b = Line_temp[l-1]
+        num2 = b[1]
+        # we will choose the point on the most right
+        if(num2 != None) and (string == 'Green'):
+            temp = [num2, j]
+            # print('\tgreen\ttemp = ', temp)
+        elif(num1 != None) and (string == 'White'):
+            temp = [num1, j]
+        # print('\t', string, '\ttemp = ', temp)
+        return temp
 
 def MidCal(a, b):
     num1 = (a - b)/2
-    num2 = b + num1
-    num2 = int(num2)
-    return num2
-
-
-#def aLine(Line):
-    #for i in range(len(Line) - 2):
-        #Pnt1 = Line[i]
-        #Pnt2 = Line[i+1]
-        #if Pnt1[0] <= Pnt2[0]:
-            #var = 0
-            #return var
+    if(a > b):
+        num2 = b + num1
+        num2 = int(num2)
+        return num2
+    elif(b > a):
+        num2 = a + num1
+        num2 = int(num2)
+        return num2
 
 
 def LineorCurve(Line):
     LoC = 0
-    aLine = 1
     if(Line != []):
         Pnt1 = Line[0]
         Pnt2 = Line[len(Line) - 1]
-        slope = (Pnt2[1] - Pnt1[1])/(Pnt2[0] - Pnt1[0])
-        const = Pnt1[1] - (Pnt1[0] * slope)
+        slope = (Pnt2[0] - Pnt1[0])/(Pnt2[1] - Pnt1[1])
+        print(Pnt1, '\t', Pnt2)
+        const1 = Pnt1[0] - (Pnt1[1] * slope)
+        const2 = Pnt2[0] - (Pnt2[1] * slope)
+        const = (const1 + const2)/2
+
+        # print("slope = ", slope, "const = ", const)
+        # calculate the slope ofthe whole line
 
         num1 = len(Line)/4
         num1 = int(num1)
-        Pnt3 = Line[num1]
-        LoC = LoCCal(Pnt3, slope, const)
-        '''
+        Pnt1 = Line[num1]
+        LoC1 = LoCCal(Pnt1, slope, const)
+        # comparing the LoC of a point at 1/4 of the line
+        
         num2 = len(Line)/2
         num2 = int(num2)
-        Pnt3 = Line[num2]
-        LoC2 = LoCCal(Pnt3, slope, const)
+        Pnt2 = Line[num2]
+        LoC2 = LoCCal(Pnt2, slope, const)
+        # comparing the LoC of a point at 1/2 of the line
 
         num3 = (len(Line)/4)*3
         num3 = int(num3)
         Pnt3 = Line[num3]
         LoC3 = LoCCal(Pnt3, slope, const)
+        # comparing the LoC of a point at 3/4 of the line
 
-        sum = LoC1 + LoC2 + LoC3
-        LoC = sum/3
-        '''
+        LoC_arr = np.array([LoC1, LoC2, LoC3])
+
+        if(np.sum(LoC_arr == 0) >= 2):
+        # if there is two or more LoC is judged as straight line
+            LoC = 0
+        elif(np.sum(LoC_arr == 1) >= 2):
+        # if there is two or more LoC is judged as curve
+            LoC = 1
+
         return LoC
     else:
         LoC = 2
+        # LoC is a situation that the line is not a straight line or a curve
         return LoC
 
 
 def LoCCal(Pnt3, slope, const):
-    y1 = Pnt3[1]
+    y1 = 0
     Pnt4 = Pnt3
-    y2 = (slope * Pnt4[0]) + const
-    diff = y1 - y2
-    if(abs(diff) < 1.5 and aLine == 0):
-            LoC = 0
+    y1 = (Pnt4[1]*slope) + const
+    y1 = int(y1)
+    diff = Pnt4[0] - y1
+    if(abs(diff) < 5):
+        LoC = 0
     else:
         LoC = 1
     return LoC
 
 
+# This function is used to calculate slope of a line
 def LineSlope(Line, LoC):
     slope = 0.0
     idx = 0
+    l = len(Line) - 1
+
+    idx1 = (len(Line))/3
+    idx1 = int(idx1)
+
+    idx2 = (len(Line))/3
+    idx2 = 2 * idx2
+    idx2 = int(idx2)
+
     if(LoC == 0):
-        idx1 = (len(Line))/4
-        idx1 = int(idx1)
-        slope1 = CalSlope(Line, idx1)
-
-        idx2 = (len(Line))/2
-        idx2 = int(idx2)
-        slope2 = CalSlope(Line, idx2)
-
-        idx3 = (len(Line))/4
-        idx3 = 3 * idx3
-        idx3 = int(idx3)
-        slope3 = CalSlope(Line, idx3)
-
-        print('slope1 = ', slope1)
-        print('slope2 = ', slope2)
-        print('slope3 = ', slope3)
-
+    # if the line is a straight line
+    # then we need to find three different pairs of points
+    # and their corresponding slope
+        slope1 = CalSlope(Line, l, idx2)
+        slope2 = CalSlope(Line, idx2, idx1)
+        slope3 = CalSlope(Line, idx1, idx)
         slope = (slope1+slope2+slope3) / 3
+        return slope
+
     elif(LoC == 1):
-        idx = (len(Line))/4
-        idx = 3 * idx
-        idx = int(idx)
-        slope = CalSlope(Line, idx)
-        print(slope)
+    # if the line is a curve
+    # then we only need to calculate the slope of the pairs at bottom
+        slope = CalSlope(Line, l, idx1)
+        # the slope that the robot uses to test, how much should the robot turn
+        return slope
     elif(LoC == 2):
+    # if the robot cannot find a line
+    # then it will give the value of LoC
+    # so that the robot will not give back an Error
         return LoC
 
-    if slope > 0.1835:#[!!!]MIGHT NOT CORRECT
-    # it should be 0.8015
-    # the PERSPECTIVE angle 0.5
-    #           plus
-    # DIFFERENCE between pi and max radians of turtlebot Burger 0.3015
-    # HOWEVER
-    # we still need to concider about the extra slope
-    # caused by the distance of the camera 0.2
-        slope = slope - 0.1565
-    elif (slope <= 0.1835) and (slope > 0.0615):
-        slope = 0.0
-    elif (slope < 0.0615):
-        slope = slope - 0.0615
-    return slope
+    # if slope > 0.1835:#[!!!]MIGHT NOT CORRECT
+    # # it should be 0.8015
+    # # the PERSPECTIVE angle 0.5
+    # #           plus
+    # # DIFFERENCE between pi and max radians of turtlebot Burger 0.3015
+    # # HOWEVER
+    # # we still need to concider about the extra slope
+    # # caused by the distance of the camera 0.2
+    #     slope = slope - 0.1565
+    # elif (slope <= 0.1835) and (slope > 0.0615):
+    #     slope = 0.0
+    # elif (slope < 0.0615):
+    #     slope = slope - 0.0615
+    # return slope
 
 
-def CalSlope(Line, idx):
-    l = len(Line) - 1
-    Pnt1 = Line[l]
-    Pnt2 = Line[idx]
-    print('we will calculate the tangent line with Point', Pnt1, 'and Point', Pnt2)
+def CalSlope(Line, idx1, idx2):
+    Pnt1 = Line[idx1] # height of Pnt1 should be lower as the one of Pnt2
+    Pnt2 = Line[idx2]
+    # print('we will calculate the tangent line with Point', Pnt1, 'and Point', Pnt2)
     slope = GetAngl(Pnt1, Pnt2)
     return slope
 
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 import cv2
+import time
 import numpy as np
-import color_detector_004 as cd
-import color_det_part1 as cd1
-import color_det_hsv_red as cd2
+import color_detector as cd
 from sensor_msgs.msg import Image
 
 #cy_max_bot = cd.LinePos(img, height, width, i_bot)[0]
@@ -27,17 +26,106 @@ def print_line(Line, img):
         cv2.circle(img, (a, temp1[1]), 2, (0, 0, 255), 2)
 
 
+def DeterHeight1(img, height, width, idx1, idx2, string1, string2):
+    if(idx1 <= idx2):
+        a = 1
+    elif(idx1 > idx2):
+        a = -1
+    for i in range(idx1, idx2, a):
+    # for idx1 < idx2: Check from upper to bottom, if there is "string" points
+    # for idx1 > idx2: Check from bottom to upper, if there is "string" points
+        J = (cd.ColDet(img, height, width, i, string1))
+        # print('J = ', J)
+        U = ((J == None)or(J == []))
+        # print(string, 'at', i, 'cd.ColDet(img,', height, ',', width, ',', i, ',', string, '):', U)
+        if((J == None)or(J == [])):
+            idx1 = i
+            # print('\t i = ', i, '\tidx1 == height-1:', idx1 == height-1)
+            if((idx1 == height-1)and(string2 == 'min')):
+                return i
+            elif((idx1 == 0)and(string2 == 'max')):
+                return i
+        else:
+            return i
+
+
+def DeterHeight2(img, height, width):
+    print('height = ', height, 'width = ', width)
+    Wminhigt = DeterHeight1(img, height, width, 0, height, "White", "min")
+    Gminhigt = DeterHeight1(img, height, width, 0, height, "Green" or "Yellow", "min")
+    print('minhigt for W:', Wminhigt, '; \tfor G:', Gminhigt)
+
+    Wmaxhigt = DeterHeight1(img, height, width, height-1, -1, "White", "max")
+    Gmaxhigt = DeterHeight1(img, height, width, height-1, -1, "Green" or "Yellow", "max")
+    print('maxhigt for W:', Wmaxhigt, '; \tfor G:', Gmaxhigt)
+
+    if(abs(Gminhigt-Gmaxhigt) <= 50)or(abs(Wminhigt-Wmaxhigt) <= 50):
+        minhigt = max(Wminhigt, Gminhigt)
+        maxhigt = max(Wmaxhigt, Gmaxhigt)
+        return minhigt, maxhigt
+
+    # minhigt = max(Wminhigt, Gminhigt) - 0
+    # maxhigt = (height-1) - min(Wmaxhigt, Gmaxhigt)
+    return Wminhigt, Wmaxhigt, Gminhigt, Gmaxhigt
+
+
+def DrawTangent(Line, img, string):
+    l = len(Line) - 1
+    idx1 = (len(Line))/6
+    idx1 = 5 * idx1
+    idx1 = int(idx1)
+    idx2 = (len(Line))/3
+    idx2 = 2 * idx2
+    idx2 = int(idx2)
+    if(string == 'bot'):
+        Pnt0 = Line[l]
+        cv2.circle(img, (Pnt0[0], Pnt0[1]), 1, (255, 0, 0), 2)
+        Pnt1 = Line[idx2]
+        cv2.circle(img, (Pnt1[0], Pnt1[1]), 1, (255, 0, 0), 4)
+        slope = (Pnt1[0] - Pnt0[0])/(Pnt1[1] - Pnt0[1])
+        print('slope = ', slope)
+        const = Pnt0[0] - (Pnt0[1] * slope)
+        print('const = ', const)
+        for i in range(120):
+            Pnt = Line[l-i]
+            print(Pnt)
+            y = Pnt[1]
+            x = Pnt[1]*slope + const
+            x = int(x)
+            print('[', x, ',', y, ']')
+            cv2.circle(img, (x, y), 1, (0, 0, 255), 2)
+        return 0
+    if(string == 'line'):
+        Pnt0 = Line[idx2]
+        cv2.circle(img, (Pnt0[0], Pnt0[1]), 1, (255, 0, 0), 2)
+        Pnt1 = Line[idx1]
+        cv2.circle(img, (Pnt1[0], Pnt1[1]), 1, (255, 0, 0), 4)
+        slope = (Pnt1[0] - Pnt0[0])/(Pnt1[1] - Pnt0[1])
+        print('slope = ', slope)
+        const = Pnt0[0] - (Pnt0[1] * slope)
+        print('const = ', const)
+        for i in range(120):
+            Pnt = Line[idx2-i]
+            print(Pnt)
+            y = Pnt[1]
+            x = Pnt[1]*slope + const
+            x = int(x)
+            print('[', x, ',', y, ']')
+            cv2.circle(img, (x, y), 1, (0, 175, 175), 2)
+
+
 def main():
-    PNGList = ["001.png", "002.png", "003.png", "004.png", "005.png", "006.png", 
-               "011.png", "012.png", "014.png", "015.png", "017.png", "019.png", 
-               "020.png", "021.png", "026.png", "027.png", "028.png", "029.png", 
-               "030.png", "033.png", "034.png", "035.png"]
+
+    PNGList = ["001.png", "002.png", "003.png", "004.png", "007.png", 
+               "008.png", "009.png", "010.png", "011.png", "012.png", 
+               "013.png", "014.png"]
     n = len(PNGList)
-    print("the amount of PNGList: ", n)
     # for i in range(0, n-1):
-    i = 18
+    start = time.time()
+    i = 4 # 14
+    print("the amount of PNGList: ", PNGList[i])
     img_cv = cv2.imread(PNGList[i])
-    print("\timported image", i, ": ", PNGList[i], '\n')
+    # print("\timported image", i, ": ", PNGList[i], '\n')
     #img_msg = cd.cv2_to_imgs2mg(cv_image)
     #img = cd.imgmsg_to_cv2(img_msg)
     height, width, _ = img_cv.shape
@@ -45,80 +133,72 @@ def main():
     print('width = ', width)
 
     i_bot = height - 5
-    i_mid = height - 40
-    width_n = width - 260
-    i = width_n
+    i_mid = height - 60
+    # height = height-1
+    # width = width-1
+    width_n = width - 250
+    # print('width_n = ', width_n)
 
     out_pnt = np.float32([[0, 0],
                         [0, height-1],
                         [width-1, height-1],
                         [width-1, 0]])
-    #print(out_pnt)
-    inp_pnt = np.float32([[width_n + 58, i_mid],
-                        [width_n + 8, i_bot],
-                        [width-1, i_bot],
-                        [300, i_mid]])
-    #print(inp_pnt)
-    maxw = width - 1 - width_n
-    maxh = height - 1 - i_mid
+    inp_pnt = np.float32([[0, i_mid], # width_n + 58
+                        [0, i_bot], # width_n + 8
+                        [width-1, i_bot], # width-1
+                        [width-1, i_mid]]) # 300
+
     M = cv2.getPerspectiveTransform(inp_pnt, out_pnt)
     img = cv2.warpPerspective(img_cv, M, (width, height), flags = cv2.INTER_LINEAR)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    height, width, _ = img.shape
+    Wminhigt = DeterHeight2(img, height, width)[0]
+    Wmaxhigt = DeterHeight2(img, height, width)[1]
+    Gminhigt = DeterHeight2(img, height, width)[2]
+    Gmaxhigt = DeterHeight2(img, height, width)[3]
 
-    #height, width, _ = img.shape
-    #print('height = ', height)
-    #print('width = ', width)
+    # print(cd.ColDet(img, 240 , 320 , 1 , "Green"))
 
-    minwid = 0
-    maxwid = height
+    Line = cd.LineForm(img, height, width, "White", "Green", Wminhigt, Wmaxhigt, Gminhigt, Gmaxhigt)
+    # GLine = cd.LineForm(img, height, width, "Green" or "Yellow", minhigt, maxhigt)
+    # GLine = cd.checkagain(img, GLine, height, width, "Green")
+    # GLine = cd.ReformLine(GLine, height)
+    # Line = cd.ActionPath(img, height, width, minhigt, maxhigt, "White", "Green" or "Yellow")
+    # Line = ReformLine(Line, height)
+    # checkagain(img, WLine, height, width, Rstring)
+    if((Line != 0)and(Line != 1)):
+        for i in range(len(Line)):
+            Pnt = Line[i]
+            # Pnt1 = WLine[i]
+            # Pnt2 = GLine[i]
+            # print('i = ', i, '; Pos = ', Pnt)
+            cv2.circle(img, (Pnt[0], Pnt[1]), 1, (255, 0, 0), 3)
+            # cv2.circle(img, (Pnt1[0], Pnt1[1]), 1, (125, 255, 0), 3)
+            # cv2.circle(img, (Pnt2[0], Pnt2[1]), 1, (0, 175, 175), 3)
 
-    # Line = cd.LinePos(img, height, width, 150, 0)
-    # print(len(Line))
+    LoC = cd.LineorCurve(Line)
+    print('LoC = ' , LoC)
+    slope = cd.LineSlope(Line, LoC)
+    if(LoC == 0):
+        # DrawTangent(WLine, img, "bot")
+        # DrawTangent(WLine, img, "line")
+        print('slope = ', slope)
+    elif(LoC == 1):
+        # DrawTangent(WLine, img, "bot")
+        # DrawTangent(WLine, img, "line")
+        print('slope = ', slope)
+    
 
-    # if(Line != []):
-    #     i = 0
-    #     for i in range(len(Line)):
-    #         Pnt = Line[i]
-    #         # print(Pnt)
-    #         cv2.circle(img, (Pnt[1], Pnt[0]), 1, (0, 255, 0), 3)
-
-    # Liner = cd2.HSV_Red_Det(img_hsv, height, width, 0, 0)
-
-    # if(Liner != []):
-    #     i = 0
-    #     for i in range(len(Liner)):
-    #         Pntr = Liner[i]
-    #         # print(Pntr)
-    #         cv2.circle(img_hsv, (Pntr[1], Pntr[0]), 1, (255, 0, 0), 3)
-
-    Line_n = cd.LineForm(img, height, width, minwid, maxwid)
-    cd1.checkagain(img, Line_n, height, width)
-
-    for i in range(len(Line_n)):
-        Pnt = Line_n[i]
-        print('i = ', i, '; Pos = ', Pnt)
-        cv2.circle(img, (Pnt[0], Pnt[1]), 1, (255, 0, 0), 5)
-
-    # # Liner_n = cd2.LinerForm(img_hsv, height, width, minwid, maxwid)
-    # cd1.checkagain(img, Line_n, height, width)
-
-    # for i in range(len(Liner_n)):
-    #     Pnt = Line_n[i]
-    #     # print('i = ', i, '; pos: ', Pnt)
-    #     cv2.circle(img_hsv, (Pnt[0], Pnt[1]), 1, (0, 0, 255), 5)
-
-    #print_line(Line, img)
-    #LoC = cd.LineorCurve(Line_n)
-    #print('LoC = ', LoC)
-    #print('slope = ', cd.LineSlope(Line_n, LoC))
-    # 004.png: 0.6514532274507854
-    # 003.png: 0.9629943306809362
-    # 001.png:
+    end = time.time()
+    print(end-start)
 
     # cv2.imshow("IMG_CV", img_cv)
     cv2.imshow("IMG", img)
+    # cv2.imshow("GIMG", Gimg)
     # cv2.imshow("IMG_HSV", img_hsv)
     cv2.waitKey(0)
+    
 
     cv2.destroyAllWindows()
 
