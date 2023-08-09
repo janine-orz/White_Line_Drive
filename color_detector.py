57aa93767ec56c98c1b874e2811ed1121f80f8fd
janine-orz <135732079+janine-orz@users.noreply.github.com>
2023-08-09 17:29:59 +0200

    Add files via upload
    finish to find the line that the robot cannot see. Bur still forward driving.

--- color_detector.py
+++ color_detector.py
@@ -16,21 +16,28 @@ def ColRec(img, i, j):
     g_val = pixel_center[1]
     r_val = pixel_center[2]
 
-    if b_val > 130 and g_val > 160 and r_val > 150:
+    if b_val > 125 and g_val > 130 and r_val > 150:
         color = "White"
-    elif b_val < 100 and g_val < 100 and r_val < 100:
+    elif b_val < 60 and g_val < 80 and r_val < 40:
         color = "Black"
     else:
+        # print("...............Searching in HSV_image...................")
         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         pixel_center = hsv_img[i, j]
         h_val = pixel_center[0]
-        if h_val < 5:
+        s_val = pixel_center[1]
+        v_val = pixel_center[2]
+        # print(h_val, s_val, v_val)
+        if(s_val < 65 and v_val < 65):
+            # print("s_val < 50 and v_val < 65")
+            color = "White"
+        elif h_val < 5:
             color = "Red"
         elif h_val < 22:
             color = "Orange"
         elif h_val < 33:
             color = "Yellow"
-        elif h_val < 78:
+        elif h_val < 80:
             color = "Green"
         elif h_val < 131:
             color = "Blue"
@@ -71,13 +78,29 @@ def LineForm(img, height, width, string1, string2, Wi_min, Wi_max, Gi_min, Gi_ma
     Line1_temp = []
     Line2_temp = []
     i_min = max(Wi_min, Gi_min)
+    print('Wi_min = ', Wi_min, '\t\tGi_min = ', Gi_min)
     i_max = min(Wi_max, Gi_max)
-    if(i_min == Gi_min): # The Greenline is shorter then the Whiteline
+    print('Wi_max = ', Wi_max, '\t\tGi_max = ', Gi_max)
+    if(i_min == Gi_min): 
+        print("i_min == Gi_min")# The lower bound of Whiteline is smaller then the Greenline
         for h in range (Wi_min, Gi_min):
             Line1_temp = ColDet(img, height, width, h, string1)
-            L1 = Lineform(Line1_temp, h, h, string1, img, height, width)
-            L2 = [0, h]
-            x_temp = MidCal(L1[0], L2[0])
+            # print('color = ', string1, '\t\tLine1_temp = ', Line1_temp)
+            L1 = Lineform(Line1_temp, L1, h, h, string1, img, height, width)
+            L2 = L1[0] - 200
+            # print('in range (0, Gi_min)\tL1 = ', L1, '\t\tL2 = ', L2)
+            x_temp = MidCal(L1[0], L2)
+            temp = [x_temp, h]
+            L.append(temp)
+    elif(i_min == Wi_min):
+        print("i_min == Wi_min")
+        for h in range (Gi_min, Wi_min):
+            Line2_temp = ColDet(img, height, width, h, string2)
+            print('color = ', string2, '\t\tLine2_temp = ', Line2_temp)
+            L2 = Lineform(Line2_temp, L2, h, h, string2, img, height, width)
+            L1 = L2[0] + 200
+            # print('in range (0, Wi_min)\tL1 = ', L1, '\t\tL2 = ', L2)
+            x_temp = MidCal(L1, L2[0])
             temp = [x_temp, h]
             L.append(temp)
     for i in range(i_min, i_max):
@@ -87,8 +110,8 @@ def LineForm(img, height, width, string1, string2, Wi_min, Wi_max, Gi_min, Gi_ma
         # print(string2, "\n")
         Line2_temp = ColDet(img, height, width, i, string2)
         # print('Line1_temp = ', Line1_temp, '\tLine2_temp = ', Line2_temp)
-        L1 = Lineform(Line1_temp, i, i, string1, img, height, width)
-        L2 = Lineform(Line2_temp, i, i, string2, img, height, width)
+        L1 = Lineform(Line1_temp, L1, i, i, string1, img, height, width)
+        L2 = Lineform(Line2_temp, L2, i, i, string2, img, height, width)
         # print('\tL1 = ', L1, '\t\tL2 = ', L2)
         x_temp = MidCal(L1[0], L2[0])
         temp = [x_temp, i]
@@ -96,21 +119,44 @@ def LineForm(img, height, width, string1, string2, Wi_min, Wi_max, Gi_min, Gi_ma
     if(i_max == Gi_max): # The Greenline is shorter then the Whiteline
         for k in range (Gi_max, Wi_max):
             Line1_temp = ColDet(img, height, width, k, string1)
-            L1 = Lineform(Line1_temp, k, k, string1, img, height, width)
-            L2 = [0, k]
-            x_temp = MidCal(L1[0], L2[0])
+            # print('color = ', string1, '\t\tLine1_temp = ', Line1_temp)
+            L1 = Lineform(Line1_temp, L1, k, k, string1, img, height, width)
+            L2 = L1[0] - 200
+            # print('in range (Gi_max, height)\twhiteline = ', L1, '\t\tgreenline = ', L2)
+            x_temp = MidCal(L1[0], L2)
+            temp = [x_temp, k]
+            L.append(temp)
+    elif(i_max == Wi_max):
+        for k in range (Wi_max, Gi_max):
+            Line2_temp = ColDet(img, height, width, k, string2)
+            print('color = ', string2, '\t\tLine2_temp = ', Line2_temp)
+            L2 = Lineform(Line2_temp, L2, k, k, string2, img, height, width)
+            L1 = L2[0] + 300
+            # print('in range (Wi_max, height)\twhiteline = ', L1, '\t\tgreenline = ', L2)
+            x_temp = MidCal(L1, L2[0])
             temp = [x_temp, k]
             L.append(temp)
     return L
 
-def Lineform(Line_temp, i, j, string, img, height, width):
+
+def Lineform(Line_temp, L, i, j, string, img, height, width):
     # print('i = ', i, ', string = ', string)
-    C = []
+    LL = []
+    LL.append(L)
     if((Line_temp == [])or(Line_temp == None)):
-        print('i = ', i, 'i-1 = ', i-1, ', string = ', string)
-        Line_temp = ColDet(img, height, width, i-1, string)
-        temp = Lineform(Line_temp, i-1, j, string, img, height, width)
-        print("\t", temp)
+        # print('i = ', i, 'i-1 = ', i-1, ', string = ', string)
+        # Line_temp = ColDet(img, height, width, i-1, string)
+        # temp = Lineform(Line_temp, i-1, j, string, img, height, width)
+        # print("\t", temp)
+        print("len L = ", len(LL), "L = ", LL)
+        if len(LL) < 2:
+            pos = L
+        else:
+            pos = L[-1]
+        # print("\tpos", pos)
+        num3 = pos[0]
+        # print("\tnum3", num3)
+        temp = [num3, j]
         return temp
     elif((Line_temp != None)or(Line_temp != [])):
         l = len(Line_temp)
@@ -124,8 +170,9 @@ def Lineform(Line_temp, i, j, string, img, height, width):
             # print('\tgreen\ttemp = ', temp)
         elif(num1 != None) and (string == 'White'):
             temp = [num1, j]
-        # print('\t', string, '\ttemp = ', temp)
+            # print('\t', string, '\ttemp = ', temp)
         return temp
+
 
 def MidCal(a, b):
     num1 = (a - b)/2
